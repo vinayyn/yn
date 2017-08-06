a858d4d335c395b526001e095c95be4bcc8d69fb
vinayyn <30504702+vinayyn@users.noreply.github.com>
2017-08-06 10:03:07 +0530

    Add files via upload
    Here I used adxl 335 for axis variation detection.When variation detected the arduino will send ATD command along with the predefined number to GSM module

---
+++ harley.ino
@@ -0,0 +1,113 @@
+#include <EEPROM.h>
+#include "EEPROMAnything.h"
+#include <LiquidCrystal.h>
+#include <SoftwareSerial.h>
+SoftwareSerial mySerial(7,8); // RX, TX
+
+const int alarmPin = 4;
+const int relayPin = 0;
+int Xacc, Yacc, Zacc, threshold = 0, thresholdSET = 25;
+
+long debouncing_time = 15; //Debouncing Time in Milliseconds
+volatile unsigned long last_micros;
+LiquidCrystal lcd(5, 6, 9, 10, 11, 12);
+struct sensorValue
+{
+  int X;
+  int Y;
+  int Z;
+};
+
+sensorValue acceleration;
+
+void debounceInterrupt_Increment()
+{
+  if ((long)(micros() - last_micros) >= debouncing_time * 1000) {
+    IncrementThreshold();
+    last_micros = micros();
+  }
+}
+
+void debounceInterrupt_Decrement()
+{
+  if ((long)(micros() - last_micros) >= debouncing_time * 1000) {
+    DecrementThreshold();
+    last_micros = micros();
+  }
+}
+void IncrementThreshold() {
+  thresholdSET = EEPROM.read(500);
+  thresholdSET++;
+  EEPROM.write(500, thresholdSET);
+}
+
+void DecrementThreshold() {
+  thresholdSET = EEPROM.read(500);
+  thresholdSET--;
+  EEPROM.write(500, thresholdSET);
+}
+
+void setup() {
+  Serial.begin(9600);
+  lcd.begin(16, 2);
+  attachInterrupt(0, debounceInterrupt_Increment, RISING);
+  attachInterrupt(1, debounceInterrupt_Decrement, RISING);
+  pinMode(alarmPin, OUTPUT);
+  pinMode(relayPin, OUTPUT);
+  digitalWrite(relayPin, HIGH);
+  EEPROM.write(500, thresholdSET);
+  digitalWrite(alarmPin, LOW);
+  lcd.setCursor(0, 0);
+  lcd.print("Initializing ");
+  lcd.setCursor(0,1);
+  lcd.print("The Setup");
+  delay(1000);
+  sensorValue acceleration = { analogRead(A0) , analogRead(A1) , analogRead(A2) };
+  EEPROM_writeAnything(0, acceleration);
+  EEPROM_readAnything(0, acceleration);
+  lcd.clear();
+}
+
+void loop() {
+  EEPROM_readAnything(0, acceleration);
+  threshold = EEPROM.read(500);
+  lcd.setCursor(0, 0);
+  lcd.print("Monitoring Mode");
+ 
+  lcd.setCursor(0,1);
+  lcd.print("Reference = ");
+  lcd.print(threshold);
+  Xacc = analogRead(A0);
+  Yacc = analogRead(A1);
+  Zacc = analogRead(A2);
+
+  if ((Xacc >= (acceleration.X + threshold)) || (Xacc <= (acceleration.X - threshold))||(Yacc >= (acceleration.Y + threshold)) || (Yacc <= (acceleration.Y - threshold))||(Zacc >= (acceleration.Z + threshold)) || (Zacc <= (acceleration.Z - threshold))) {
+    digitalWrite(relayPin, LOW);
+    digitalWrite(alarmPin,HIGH);
+  Serial.println("alarm!!!");
+    mySerial.begin(9600);
+    delay(500);
+    mySerial.println("ATD9845730447;"); // ATDxxxxxxxxxx; -- watch out here for semicolon at the end!!
+  Serial.println("Called ATD9845730447");
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("The sensor sensed");
+    lcd.setCursor(0,1);
+    lcd.print("Axis variations");
+    delay(2000);
+     lcd.clear();
+     lcd.setCursor(0, 0);
+    lcd.print("GSM modem calling");
+    lcd.setCursor(0,1);
+    lcd.print("NO +919845730447");
+    delay(7000);
+    digitalWrite(relayPin, HIGH);
+    digitalWrite(alarmPin,LOW);
+    lcd.clear();
+
+    }
+    
+  }
+ 
+
+
